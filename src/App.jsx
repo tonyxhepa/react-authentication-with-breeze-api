086b6b1b4ecd715b983f0cc1fb3ec95603a8b74b
tony xhepa <tonyxhepa@tonys-iMac.local>
2022-12-07 14:25:07 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="bg-slate-100 min-h-screen">
+      <nav className="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <a href="https://laraveller.com/" className="flex items-center">
+            Laraveller
+          </a>
+          <div className="hidden w-full md:block md:w-auto">
+            <ul
+              className="
+            mt-4
+            flex flex-col
+            rounded-lg
+            p-4
+            md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium
+          "
+            >
+              <li>
+                <Link
+                  to="/"
+                  className="block rounded py-2 pr-4 pl-3 text-white"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/login"
+                  className="block rounded py-2 pr-4 pl-3 text-white"
+                  aria-current="page"
+                >
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/register"
+                  className="block rounded py-2 pr-4 pl-3 text-white"
+                  aria-current="page"
+                >
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="max-w-7xl mx-auto mt-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,160 @@
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <section className="bg-[#F4F7FF] py-20 lg:py-[120px]">
+      <div className="container mx-auto">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div
+              className="
+              relative
+              mx-auto
+              max-w-[525px]
+              overflow-hidden
+              rounded-lg
+              bg-white
+              py-16
+              px-10
+              text-center
+              sm:px-12
+              md:px-[60px]
+            "
+            >
+              <div className="mb-10 text-center md:mb-16">Laraveller</div>
+              <form>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="
+                    bordder-[#E9EDF4]
+                    w-full
+                    rounded-md
+                    border
+                    bg-[#FCFDFE]
+                    py-3
+                    px-5
+                    text-base text-body-color
+                    placeholder-[#ACB6BE]
+                    outline-none
+                    focus:border-primary
+                    focus-visible:shadow-none
+                  "
+                  />
+                  <div className="flex">
+                    <span className="text-red-400 text-sm m-2 p-2">error</span>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="
+                    bordder-[#E9EDF4]
+                    w-full
+                    rounded-md
+                    border
+                    bg-[#FCFDFE]
+                    py-3
+                    px-5
+                    text-base text-body-color
+                    placeholder-[#ACB6BE]
+                    outline-none
+                    focus:border-primary
+                    focus-visible:shadow-none
+                  "
+                  />
+                  <div className="flex">
+                    <span className="text-red-400 text-sm m-2 p-2">error</span>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    className="
+                    bordder-[#E9EDF4]
+                    w-full
+                    rounded-md
+                    border
+                    bg-[#FCFDFE]
+                    py-3
+                    px-5
+                    text-base text-body-color
+                    placeholder-[#ACB6BE]
+                    outline-none
+                    focus:border-primary
+                    focus-visible:shadow-none
+                  "
+                  />
+                  <div className="flex">
+                    <span className="text-red-400 text-sm m-2 p-2">error</span>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="password"
+                    placeholder="Password Confirmation"
+                    className="
+                    bordder-[#E9EDF4]
+                    w-full
+                    rounded-md
+                    border
+                    bg-[#FCFDFE]
+                    py-3
+                    px-5
+                    text-base text-body-color
+                    placeholder-[#ACB6BE]
+                    outline-none
+                    focus:border-primary
+                    focus-visible:shadow-none
+                  "
+                  />
+                  <div className="flex">
+                    <span className="text-red-400 text-sm m-2 p-2">error</span>
+                  </div>
+                </div>
+                <div className="mb-10">
+                  <button
+                    type="submit"
+                    className="
+                    w-full
+                    px-4
+                    py-3
+                    bg-indigo-500
+                    hover:bg-indigo-700
+                    rounded-md
+                    text-white
+                  "
+                  >
+                    Register
+                  </button>
+                </div>
+              </form>
+              <Link
+                to="/forgot-password"
+                className="
+                mb-2
+                inline-block
+                text-base text-[#adadad]
+                hover:text-primary hover:underline
+              "
+              >
+                Forgot Password?
+              </Link>
+              <p className="text-base text-[#adadad]">
+                Not a member yet?
+                <Link to="/register" className="text-primary hover:underline">
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
