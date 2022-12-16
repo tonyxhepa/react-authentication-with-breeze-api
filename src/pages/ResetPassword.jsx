484eeb35c89eb469e19ce0e142322bc8a62a8325
laraveller <laraveller2021@gmail.com>
2022-12-16 11:03:07 +0100

    add reset-password

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,11 +1,149 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import axios from "../api/axios";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams, Link } from "react-router-dom";
 
 const ResetPassword = () => {
   const { token } = useParams();
   const [searchParams] = useSearchParams();
-  const email = searchParams.get("email");
-  console.log(token, email);
-  return <div>ResetPassword</div>;
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [status, setStatus] = useState(null);
+
+  useEffect(() => {
+    setEmail(searchParams.get("email"));
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/reset-password", {
+        token,
+        email,
+        password,
+        password_confirmation,
+      });
+      setStatus(response.data.status);
+    } catch (e) {
+      if (e.response.status === 422) {
+        setErrors(e.response.data.errors);
+      }
+    }
+  };
+  return (
+    <section className="bg-[#F4F7FF] py-20 lg:py-[120px]">
+      <div className="container mx-auto">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div
+              className="
+              relative
+              mx-auto
+              max-w-[525px]
+              overflow-hidden
+              rounded-lg
+              bg-white
+              py-16
+              px-10
+              text-center
+              sm:px-12
+              md:px-[60px]
+            "
+            >
+              {status && (
+                <div className="bg-green-700 p-2 m-2 rounded-md text-white">
+                  {status}
+                </div>
+              )}
+              {errors.email && (
+                <div className="bg-red-700 p-2 m-2 rounded-md text-white">
+                  {errors.email[0]}
+                </div>
+              )}
+              <div className="mb-10 text-center md:mb-16">
+                Add Your new Password
+              </div>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    className="
+                    bordder-[#E9EDF4]
+                    w-full
+                    rounded-md
+                    border
+                    bg-[#FCFDFE]
+                    py-3
+                    px-5
+                    text-base text-body-color
+                    placeholder-[#ACB6BE]
+                    outline-none
+                    focus:border-primary
+                    focus-visible:shadow-none
+                  "
+                  />
+                  {errors.password && (
+                    <div className="flex">
+                      <span className="text-red-400 text-sm m-2 p-2">
+                        {errors.password[0]}
+                      </span>
+                    </div>
+                  )}
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="password"
+                    value={password_confirmation}
+                    onChange={(e) => setPasswordConfirmation(e.target.value)}
+                    placeholder="Password Confirmation"
+                    className="
+                    bordder-[#E9EDF4]
+                    w-full
+                    rounded-md
+                    border
+                    bg-[#FCFDFE]
+                    py-3
+                    px-5
+                    text-base text-body-color
+                    placeholder-[#ACB6BE]
+                    outline-none
+                    focus:border-primary
+                    focus-visible:shadow-none
+                  "
+                  />
+                </div>
+                <div className="mb-10">
+                  <button
+                    type="submit"
+                    className="
+                    w-full
+                    px-4
+                    py-3
+                    bg-indigo-500
+                    hover:bg-indigo-700
+                    rounded-md
+                    text-white
+                  "
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+              <p className="text-base text-[#adadad]">
+                <Link to="/login" className="text-primary hover:underline">
+                  Sign in
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ResetPassword;
